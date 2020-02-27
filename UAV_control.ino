1e96fc01d886d01e0feae60feefe5f6d18ff7f44
YoungPenguin <56176145+YoungPenguin@users.noreply.github.com>
2020-02-27 21:31:35 +0100

    Update UAV_control.ino

--- UAV_control.ino
+++ UAV_control.ino
@@ -8,8 +8,11 @@
  *            (front)
  */
 
+ //read angles and forces from gyroaccelerometer
+
 #include <Wire.h>
 #include <Servo.h>
+#include "MPU9250.h"
 
 Servo prop__1;
 Servo prop__2;
@@ -33,6 +36,8 @@ int gyro_error = 0;                       //We use this variable to only calcula
 float Gyr_rawX, Gyr_rawY, Gyr_rawZ;       //Here we store the raw data read
 float Gyro_angle_x, Gyro_angle_y;         //Here we store the angle value obtained with Gyro data
 float Gyro_raw_error_x, Gyro_raw_error_y; //Here we store the initial gyro data error
+float difference=0;
+float main_loop_timer = 0;
 
 //Acc Variables
 int acc_error = 0;                          //We use this variable to only calculate once the Acc data error
@@ -54,9 +59,9 @@ float roll_pid_p = 0;
 float roll_pid_i = 0;
 float roll_pid_d = 0;
 ///////////////////////////////ROLL PID CONSTANTS////////////////////
-double roll_kp = 0.7;         //3.55
-double roll_ki = 0.006;       //0.003
-double roll_kd = 1.2;         //2.05
+double roll_kp = 3.2;         //3.55
+double roll_ki = 0.06;       //0.003
+double roll_kd = 0.014;//.2;         //2.05
 float roll_desired_angle = 0; //This is the angle in which we whant the
 
 //////////////////////////////PID FOR PITCH//////////////////////////
@@ -65,13 +70,17 @@ float pitch_pid_p = 0;
 float pitch_pid_i = 0;
 float pitch_pid_d = 0;
 ///////////////////////////////PITCH PID CONSTANTS///////////////////
-double pitch_kp = 0.72;        //3.55
-double pitch_ki = 0.006;       //0.003
-double pitch_kd = 1.22;        //2.05
+double pitch_kp = 0.1;       //3.55
+double pitch_ki = 0.03;       //0.003
+double pitch_kd = 80.0;//.22;        //2.05
 float pitch_desired_angle = 0; //This is the angle in which we whant the
 float yaw_desired_angle = 0;
 
+MPU9250 IMU(Wire,0x68);
+int status;
+
 void setup() {
+
 
   pinMode(14, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(14), blink, CHANGE);
@@ -85,12 +94,12 @@ void setup() {
   DDRB |= B00100000;  //D13 as output
   PORTB &= B11011111; //D13 set to LOW
 
-  prop__1.attach(5);
-  prop__2.attach(3);
-  prop__3.attach(4);
-  prop__4.attach(7);
-  prop__5.attach(2);
-  prop__6.attach(6);
+  prop__1.attach(3);
+  prop__2.attach(7);
+  prop__3.attach(6);
+  prop__4.attach(4);
+  prop__5.attach(5);
+  prop__6.attach(2);
 
   prop__1.writeMicroseconds(1000);
   prop__2.writeMicroseconds(1000);
@@ -100,79 +109,28 @@ void setup() {
   prop__6.writeMicroseconds(1000);
 
 ////////////////////////////////////////////////////////////////////////////////////
-  Wire.begin();                 //begin the wire comunication
-  Wire.beginTransmission(0x68); //begin, Send the slave adress (in this case 68)
-  Wire.write(0x6B);             //make the reset (place a 0 into the 6B register)
-  Wire.write(0x00);
-  Wire.endTransmission(true); //end the transmission
+  
+  // serial to display data
+  Serial.begin(115200);
+  while(!Serial) {}
 
-  Wire.beginTransmission(0x68); //begin, Send the slave adress (in this case 68)
-  Wire.write(0x1B);             //We want to write to the GYRO_CONFIG register (1B hex)
-  Wire.write(0x10);             //Set the register bits as 00010000 (100dps full scale)
-  Wire.endTransmission(true);   //End the transmission with the gyro
+  // start communication with IMU 
+  status = IMU.begin();
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+  }
 
-  Wire.beginTransmission(0x68); //Start communication with the address found during search.
-  Wire.write(0x1C);             //We want to write to the ACCEL_CONFIG register (1A hex)
-  Wire.write(0x10);             //Set the register bits as 00010000 (+/- 8g full scale range)
-  Wire.endTransmission(true);
 
-  Serial.begin(9600);
-  delay(1000);
-  time = millis(); //Start counting time in milliseconds
-
-  if (gyro_error == 0)
-  {
-    for (int i = 0; i < 200; i++)
-    {
-      Wire.beginTransmission(0x68); //begin, Send the slave adress (in this case 68)
-      Wire.write(0x43);             //First adress of the Gyro data
-      Wire.endTransmission(false);
-      Wire.requestFrom(0x68, 4, true); //We ask for just 4 registers
-
-      Gyr_rawX = Wire.read() << 8 | Wire.read(); //Once again we shif and sum
-      Gyr_rawY = Wire.read() << 8 | Wire.read();
-
-      /*---X---*/
-      Gyro_raw_error_x = Gyro_raw_error_x + (Gyr_rawX / 32.8);
-      /*---Y---*/
-      Gyro_raw_error_y = Gyro_raw_error_y + (Gyr_rawY / 32.8);
-      if (i == 199)
-      {
-        Gyro_raw_error_x = Gyro_raw_error_x / 200;
-        Gyro_raw_error_y = Gyro_raw_error_y / 200;
-        gyro_error = 1;
-      }
-    }
-  } 
-
-  if (acc_error == 0)
-  {
-    for (int a = 0; a < 200; a++)
-    {
-      Wire.beginTransmission(0x68);
-      Wire.write(0x3B); 
-      Wire.endTransmission(false);
-      Wire.requestFrom(0x68, 6, true);
-
-      Acc_rawX = (Wire.read() << 8 | Wire.read()) / 4096.0; 
-      Acc_rawY = (Wire.read() << 8 | Wire.read()) / 4096.0;
-      Acc_rawZ = (Wire.read() << 8 | Wire.read()) / 4096.0;
-
-      /*---X---*/
-      Acc_angle_error_x = Acc_angle_error_x + ((atan((Acc_rawY) / sqrt(pow((Acc_rawX), 2) + pow((Acc_rawZ), 2))) * rad_to_deg));
-      /*---Y---*/
-      Acc_angle_error_y = Acc_angle_error_y + ((atan(-1 * (Acc_rawX) / sqrt(pow((Acc_rawY), 2) + pow((Acc_rawZ), 2))) * rad_to_deg));
-
-      if (a == 199)
-      {
-        Acc_angle_error_x = Acc_angle_error_x / 200;
-        Acc_angle_error_y = Acc_angle_error_y / 200;
-        acc_error = 1;
-      }
-    }
-  } //end of acc error calculation
-} //end of setup loop
-
+Gyro_raw_error_x=0;
+Gyro_raw_error_y=0;
+Acc_angle_error_x=0;
+Acc_angle_error_y=0;
+ 
+}
 void loop()
 {
 
@@ -182,12 +140,14 @@ void loop()
   elapsedTime = (time - timePrev) / 1000;
 
   //////////////////////////////////////Gyro read/////////////////////////////////////
-  Wire.beginTransmission(0x68); 
-  Wire.write(0x43);             
-  Wire.endTransmission(false);
-  Wire.requestFrom(0x68, 4, true);         
-  Gyr_rawX = Wire.read() << 8 | Wire.read(); 
-  Gyr_rawY = Wire.read() << 8 | Wire.read();
+
+  IMU.readSensor();
+
+ 
+
+  Gyr_rawX = IMU.getGyroX_rads();
+  Gyr_rawY = IMU.getGyroY_rads();
+  
    /*---X---*/
   Gyr_rawX = (Gyr_rawX / 32.8) - Gyro_raw_error_x;
   /*---Y---*/
@@ -199,14 +159,12 @@ void loop()
   Gyro_angle_y = Gyr_rawY * elapsedTime;
 
   //////////////////////////////////////Acc read/////////////////////////////////////
-  Wire.beginTransmission(0x68);   
-  Wire.write(0x3B);               
-  Wire.endTransmission(false);   
-  Wire.requestFrom(0x68, 6, true); 
-
-  Acc_rawX = (Wire.read() << 8 | Wire.read()) / 4096.0; 
-  Acc_rawY = (Wire.read() << 8 | Wire.read()) / 4096.0;
-  Acc_rawZ = (Wire.read() << 8 | Wire.read()) / 4096.0;
+ 
+  //val her ændres alt for meget
+  Acc_rawX = IMU.getAccelX_mss();
+  Acc_rawY = IMU.getAccelY_mss();
+  Acc_rawZ = IMU.getAccelZ_mss();
+  
 
   /*---X---*/
   Acc_angle_x = (atan((Acc_rawY) / sqrt(pow((Acc_rawX), 2) + pow((Acc_rawZ), 2))) * rad_to_deg) - Acc_angle_error_x;
@@ -224,83 +182,65 @@ void loop()
   yaw_desired_angle = map(input_YAW, 1000, 2000, -20, 20);
 
   /*///////////////////////////P I D///////////////////////////////////*/
-
-  Serial.print("Input Throttle: ");
-  Serial.print(input_THROTTLE);
-  Serial.print(" ");
-
-  Serial.print("Input Pitch: ");
-  Serial.print(input_PITCH);
-  Serial.print(" ");
-
-  Serial.print("Input Roll: ");
-  Serial.print(input_ROLL);
-  Serial.print(" ");
-
-  Serial.print("Input yaw: ");
-  Serial.print(input_YAW);
-  Serial.print(" ");
-  
-  Serial.println();
   
   roll_error = Total_angle_y - roll_desired_angle;
-  pitch_error = Total_angle_x - pitch_desired_angle;
+  pitch_error = Total_angle_x - 0;
 
   roll_pid_p = roll_kp * roll_error;
   pitch_pid_p = pitch_kp * pitch_error;
+ pitch_pid_i = pitch_pid_i+(pitch_ki*pitch_error);  
 
-  if (-3 < roll_error < 3) {
-    roll_pid_i = roll_pid_i + (roll_ki * roll_error);
-  }
-  if (-3 < pitch_error < 3) {
-    pitch_pid_i = pitch_pid_i + (pitch_ki * pitch_error);
-  }
+pitch_pid_i =anti_windup(pitch_pid_i, -2, 2);
 
-  roll_pid_d = roll_kd * ((roll_error - roll_previous_error) / elapsedTime);
-  pitch_pid_d = pitch_kd * ((pitch_error - pitch_previous_error) / elapsedTime);
+
+
+  roll_pid_d = roll_kd * ((roll_error - roll_previous_error) );
+  pitch_pid_d = pitch_kd * ((pitch_error - pitch_previous_error));
 
   roll_PID = roll_pid_p + roll_pid_i + roll_pid_d;
   pitch_PID = pitch_pid_p + pitch_pid_i + pitch_pid_d;
 
   /*///////////////////////////P I D///////////////////////////////////*/
 
-  anti_windup(roll_PID, -400, 400);
-  anti_windup(pitch_PID, -400, 400);
+  roll_PID = anti_windup(roll_PID, -400, 400);
+  pitch_PID = anti_windup(pitch_PID, -400, 400);
 
   /* hex-x config */
-  pwm_1 = input_THROTTLE - roll_desired_angle - 1.732 * pitch_desired_angle - yaw_desired_angle;
+  pwm_1 = input_THROTTLE - roll_desired_angle - 1.732 * pitch_PID - yaw_desired_angle;
   pwm_2 = input_THROTTLE - 1 / 2 * roll_desired_angle + yaw_desired_angle;
-  pwm_3 = input_THROTTLE - roll_desired_angle + 1.732 * pitch_desired_angle - yaw_desired_angle;
-  pwm_4 = input_THROTTLE + roll_desired_angle + 1.732 * pitch_desired_angle + yaw_desired_angle;
+  pwm_3 = input_THROTTLE - roll_desired_angle + 1.732 * pitch_PID - yaw_desired_angle;
+  pwm_4 = input_THROTTLE + roll_desired_angle + 1.732 * pitch_PID + yaw_desired_angle;
   pwm_5 = input_THROTTLE + 1 / 2 * roll_PID - yaw_desired_angle;
-  pwm_6 = input_THROTTLE + roll_desired_angle - 1.732 * pitch_desired_angle + yaw_desired_angle;
+  pwm_6 = input_THROTTLE + roll_desired_angle - 1.732 * pitch_PID + yaw_desired_angle;
 
-  anti_windup(pwm_1, 1000, 2000);
-  anti_windup(pwm_2, 1000, 2000);
-  anti_windup(pwm_3, 1000, 2000);
-  anti_windup(pwm_4, 1000, 2000);
-  anti_windup(pwm_5, 1000, 2000);
-  anti_windup(pwm_6, 1000, 2000);
+  pwm_1 = anti_windup(pwm_1, 1000, 2000);
+  pwm_2 = anti_windup(pwm_2, 1000, 2000);
+  pwm_3 = anti_windup(pwm_3, 1000, 2000);
+  pwm_4 = anti_windup(pwm_4, 1000, 2000);
+  pwm_5 = anti_windup(pwm_5, 1000, 2000);
+  pwm_6 = anti_windup(pwm_6, 1000, 2000);
 
   if (input_THROTTLE > 1020) {
     prop__1.writeMicroseconds(pwm_1);
-    prop__2.writeMicroseconds(pwm_2);
+    prop__2.writeMicroseconds(1000);
     prop__3.writeMicroseconds(pwm_3);
     prop__4.writeMicroseconds(pwm_4);
-    prop__5.writeMicroseconds(pwm_5);
+    prop__5.writeMicroseconds(1000);
     prop__6.writeMicroseconds(pwm_6);
+
   
   }
   if (input_THROTTLE < 1000) {
    stopAll();
   }
+ pitch_previous_error = pitch_error; 
 }
 
 void blink() {
 
   current_count = micros();
   ///////////////////////////////////////Channel 1
-  if (GPIOD_PDIR & 2) { //pin 34, bit position 26
+  if (GPIOB_PDIR & 1) { //pin 16
      
     if (last_CH1_state == 0) {                         
       last_CH1_state = 1;        //Store the current state into the last state for the next loop
@@ -313,7 +253,7 @@ void blink() {
   }
 
   ///////////////////////////////////////Channel 2
-  if (GPIOC_PDIR & 1) { //pin 35, bit position 8                  D9 -- B00000010
+  if (GPIOB_PDIR & 2) { //pin 17
     if (last_CH2_state == 0) {
       last_CH2_state = 1;
       counter_2 = current_count;
@@ -321,12 +261,12 @@ void blink() {
   }
   else if (last_CH2_state == 1) {
     last_CH2_state = 0;
-    input_PITCH = current_count - counter_2;
+    input_ROLL = current_count - counter_2;
   }
 
   ///////////////////////////////////////Channel 3
   
-  if (GPIOB_PDIR & 1) {//pin 36, bit position 9                    pin D10 - B00000100
+  if (GPIOD_PDIR & 2) {//pin 14                  pin D10 - B00000100
     if (last_CH3_state == 0) {
       last_CH3_state = 1;
       counter_3 = current_count;
@@ -334,12 +274,12 @@ void blink() {
   }
   else if (last_CH3_state == 1) {
     last_CH3_state = 0;
-    input_ROLL = current_count - counter_3;
+    input_PITCH = current_count - counter_3;
   }
 
 
   ///////////////////////////////////////Channel 4
-  if (GPIOB_PDIR & 2) { //pin 37, bit position 10                                                     pin D12  -- B00010000
+  if (GPIOC_PDIR & 1) { //pin 15
     if (last_CH4_state == 0)  {
       last_CH4_state = 1;
       counter_4 = current_count;
@@ -351,7 +291,7 @@ void blink() {
   }
 }
 
-void anti_windup(float a, float b, float c) {
+int anti_windup(float a, float b, float c) {
   if (a < b) {
     a = b;
   }
