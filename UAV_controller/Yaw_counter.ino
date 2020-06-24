a0633085e34e2e048302b82d69e2294a0187d774
youngpenguin <56176145+YoungPenguin@users.noreply.github.com>
2020-06-24 18:57:18 +0200

    refactor

--- UAV_controller/Yaw_counter.ino
+++
@@ -1,6 +0,0 @@
-float Yaw_counter(float yaw_difference) {
-  yaw = (yaw_difference < -20) ? 1 : yaw_difference;
-  yaw = (yaw_difference > 20) ? -1 : yaw_difference;
-  total_yaw += yaw;
-  return total_yaw;
-}
--- UAV_controller/flightMode0.ino
+++
@@ -1,7 +0,0 @@
-void flightMode0() {
-  if ((input_pin[0] < 1000) && (input_pin[1] < 1100) && (input_pin[2] > 1700) && (input_pin[3] < 1300) && (imu.available())) {
-    flightMode = 1;
-    PORTB |=B00100000;
-  }
-  stopAll();
-}
